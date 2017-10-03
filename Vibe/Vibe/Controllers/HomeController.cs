1570aab0bb03cdefed54f630fea79715acd7c213
okocsis90 <o.kocsis90@gmail.com>
2017-10-03 12:48:15 +0200

    basic template for webpage created

---
+++ Vibe/Vibe/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vibe.Models;
+
+namespace Vibe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["BandMembers"] = new List<BandMember>();
+            ViewData["BandMembers"] = CreateBandMembersVibe();
+            return View();
+        }
+
+        private List<BandMember> CreateBandMembersVibe()
+        {
+            BandMember Zsolt = new BandMember("Zsolt", "Rónai", "Gitár", "/Content/Images/bandmember.jpg");
+            BandMember Zoli = new BandMember("Zoltán", "Papp",  "Basszusgitár", "/Content/Images/bandmember.jpg");
+            BandMember Szumpi = new BandMember("Ákos", "Szumper", "Dob", "/Content/Images/bandmember.jpg");
+            BandMember Olivér = new BandMember("Olivér", "Kocsis", "Billentyű", "/Content/Images/bandmember.jpg");
+            return new List<BandMember> { Zsolt, Zoli, Szumpi, Olivér };
+        }
+    }
+}
---
+++ Vibe/Vibe/Models/BandMember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vibe.Models
+{
+    public class BandMember
+    {
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public string ImgPath { get; set; }
+        public string HtmlId { get; set; }
+
+        public BandMember(string firstName, string lastName, string description, string img)
+        {
+            FullName = lastName + " " + firstName;
+            Description = description;
+            ImgPath = img;
+            HtmlId = "#" + firstName + "Info";
+        }
+    }
+}
